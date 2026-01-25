02c5584c68772a7c5c1f86be8e748fc8a3507edd
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-25 19:55:53 +0530

    feat: add HeroSection and FeaturedSection to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection.jsx'
+import FeaturedSection from '../components/FeaturedSection.jsx'
 
 const Home = () => {
   return (
     <div>
-      
+      <HeroSection />
+      <FeaturedSection />
     </div>
   )
 }
