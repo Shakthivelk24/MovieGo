b5b64a7ac3031467f907ff9727060f758d7330d7
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-26 20:34:40 +0530

    feat: add TrailerSection component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection.jsx'
 import FeaturedSection from '../components/FeaturedSection.jsx'
+import TrailerSection from '../components/TrailerSection.jsx'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <FeaturedSection />
+      <TrailerSection />
     </div>
   )
 }
