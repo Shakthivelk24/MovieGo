410cf39591c477c32ecbfb3874b1f1a397062f01
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-29 17:46:21 +0530

    feat: update AdminNavbar to include logo and title within the Link component

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -5,8 +5,9 @@ import { assets } from '../../assets/assets'
 const AdminNavbar = () => {
   return (
     <div className='flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30'>
-        <Link to = "/">
+        <Link to = "/" className='flex items-center gap-2'>
           <img src={assets.logo} alt="" className='w-20 h-auto' />
+          <p className='text-2xl font-bold text-white'><span className='text-red-400 text-3xl'>M</span>ovieGo</p>
         </Link>
     </div>
   )
