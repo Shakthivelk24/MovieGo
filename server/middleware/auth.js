70f38f6de0dcaa6ca4ab9eb6909e77a4259475a0
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-02 21:22:06 +0530

    fix: update response status for access denied in protectAdmin middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ export const protectAdmin = async (req,res,next) => {
         const {userId} = req.auth();
         const user = await clerkClient.users.getUser(userId)
         if(user.privateMetadata.role !== 'admin'){
-            return res.status(403).json({success:false,message:"Access denied. Admins only."})
+            return res.json({success:false,message:"Access denied. Admins only."})
         }
         next();
     } catch (error) {
