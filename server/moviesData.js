473bdfa11c2f21a10b154c09b379097af49eb832
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-02 19:21:46 +0530

    feat: add movie data to moviesData.js

---
+++ server/moviesData.js
@@ -0,0 +1,523 @@
+const data = [
+  {
+    adult: false,
+    backdrop_path:
+      "https://tse3.mm.bing.net/th/id/OIP.M3BhIn1kC58uIcsuMIHaeQHaLH?w=780&h=1170&rs=1&pid=ImgDetMain&o=7&rm=3",
+    genre_ids: [18, 12],
+    id: 201001,
+    original_language: "kn",
+    original_title: "777 Charlie",
+    overview: "A lonely man builds a deep bond with a dog.",
+    popularity: 310.45,
+    poster_path:
+      "https://tse3.mm.bing.net/th/id/OIP.M3BhIn1kC58uIcsuMIHaeQHaLH?w=780&h=1170&rs=1&pid=ImgDetMain&o=7&rm=3",
+    release_date: "2022-06-10",
+    title: "777 Charlie",
+    video: false,
+    vote_average: 8.7,
+    vote_count: 4200,
+    credits: {
+      cast: [
+        {
+          name: "Rakshit Shetty",
+          character: "Dharma",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.cd2f2730d2024d8cb2a55ac1eebbc643?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+        {
+          name: "Charlie",
+          character: "Dog",
+          profile_path:
+            "https://tse1.mm.bing.net/th/id/OIP.UYeoF2a9hOHFzwDkfJBGBAHaEG?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+        {
+          name: "Bobby Simha",
+          character: "Dog",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.57a601ce052c89e0aee5bcca8009c8a3?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+      crew: [
+        {
+          name: "Kiranraj K",
+          job: "Director",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.Pr364oYHzxp9yy64m7J2MCrA_fbOyRnB5YSs4jUGhi8?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path: "https://wallpaperaccess.com/full/2491747.jpg",
+    genre_ids: [28, 18, 53],
+    id: 201002,
+    original_language: "kn",
+    original_title: "KGF Chapter 1",
+    overview: "Rise of a rebel in Kolar gold mines.",
+    popularity: 890.32,
+    poster_path: "https://wallpaperaccess.com/full/2491747.jpg",
+    release_date: "2018-12-21",
+    title: "KGF",
+    video: false,
+    vote_average: 8.2,
+    vote_count: 9800,
+    credits: {
+      cast: [
+        {
+          name: "Yash",
+          character: "Rocky",
+          profile_path:
+            "https://tse3.mm.bing.net/th/id/OIP.iuPh9Vx_4ufP2rCnem0ujQHaKn?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+        {
+          name: "Srinidhi Shetty",
+          character: "Reena",
+          profile_path:
+            "https://static.toiimg.com/photo/87184810/87184810.jpg?imgsize-23980",
+        },
+        {
+          name: "Ramachandra Raju",
+          character: "Garuda",
+          profile_path:
+            "https://tse3.mm.bing.net/th/id/OIP.X9Rkc5bGDzdoLVVi2D9ABwHaJ9?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+      crew: [
+        {
+          name: "Prashanth Neel",
+          job: "Director",
+          profile_path:
+            "https://tse4.mm.bing.net/th/id/OIP.cYcPMHVTseYQvsHi15yeVwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://images.justwatch.com/poster/332215539/s718/jana-nayagan.jpg",
+    genre_ids: [28, 18],
+    id: 201003,
+    original_language: "ta",
+    original_title: "Jana Nayagan",
+    overview: "Leader rises against injustice.",
+    popularity: 450.12,
+    poster_path:
+      "https://images.justwatch.com/poster/332215539/s718/jana-nayagan.jpg",
+    release_date: "2026-02-14",
+    title: "Jana Nayagan",
+    video: false,
+    vote_average: 7.9,
+    vote_count: 1200,
+    credits: {
+      cast: [
+        {
+          name: "Vijay",
+          character: "Thalapathy Vetri Kondan",
+          profile_path:
+            "https://m.media-amazon.com/images/M/MV5BZWJlODhlMzctOTU0Yi00MTUwLTkxODYtMDNjNTQxYTI2YTE1XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+        },
+        {
+          name: "Pooja Hegde",
+          character: "Kayal",
+          profile_path:
+            "https://tse4.mm.bing.net/th/id/OIP.UFJUNRV_Q8E8nAMoKBeIWAHaF7?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+        {
+          name: "Bobby Deol",
+          character: "Villen",
+          profile_path:
+            "https://tse1.explicit.bing.net/th/id/OIP.KKfMudRls3KwODqhSLryRAHaJb?rs=1&pid=ImgDetMain&o=7&rm=3https://www.cinejosh.com/newsimg/newsmainimg/bobby-deol-speaks-about-his-anarchic-scene-in-animal_b_1212230748.jpg",
+        },
+      ],
+      crew: [
+        {
+          name: "H. Vinoth",
+          job: "Director",
+          profile_path:
+            "https://th.bing.com/th/id/OIP.KrT3NsIyCv-oPrJRj-0f5QHaFU?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://th.bing.com/th/id/OIP.gNs-4jojBD9nBm49K1KtHwHaKs?w=115&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    genre_ids: [35, 18],
+    id: 201004,
+    original_language: "ta",
+    original_title: "Dude",
+    overview: "Carefree man learns responsibility.",
+    popularity: 210.88,
+    poster_path:
+      "https://th.bing.com/th/id/OIP.gNs-4jojBD9nBm49K1KtHwHaKs?w=115&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    release_date: "2025-10-19",
+    title: "Dude",
+    video: false,
+    vote_average: 6.9,
+    vote_count: 640,
+    credits: {
+      cast: [
+        {
+          name: "Pradeep Ranganathan",
+          character: "Agan",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.XLkuFWgMAIBzTy6lOGT3ccH29LXoQk7lizo3RjAOo14?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+        {
+          name: "Mamitha Baiju",
+          character: "Kural",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.EeSWJ1yZZad726Z-HjbBTpLVVoEsa9W_Dsnxxrxpzl0?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+        {
+          name: "R. Sarathkumar",
+          character: "Athiyamaan",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.AA1b0bXG1n0b0g0g4Y9vWJMEP6q1Y5v8XG9p8vVvV3o?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+      crew: [
+        {
+          name: "Keerthiswaran",
+          job: "Director",
+          profile_path:
+            "https://www.thenewsmedium.com/wp-content/uploads/2025/03/Keerthiswaran-Director.webp",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://th.bing.com/th/id/OIP.exTWn6Uw2EsUlQd6T4X9GQHaKs?w=119&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    genre_ids: [28, 18, 14],
+    id: 201005,
+    original_language: "kn",
+    original_title: "Kantara Chapter 1",
+    overview: "Ancient traditions and divine roots.",
+    popularity: 720.55,
+    poster_path:
+      "https://th.bing.com/th/id/OIP.exTWn6Uw2EsUlQd6T4X9GQHaKs?w=119&h=180&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3",
+    release_date: "2025-10-02",
+    title: "Kantara Chapter 1",
+    video: false,
+    vote_average: 8.4,
+    vote_count: 1900,
+    credits: {
+      cast: [
+        {
+          name: "Rishab Shetty",
+          character: "Berme",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.8bq9m4x1NvuEhtHVrYVF8RKnmJav4Ph_AhYiKdOhtg4?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+        {
+          name: "Rukmini Vasanth",
+          character: "Kanakavathi",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.4c52b4b16331df1bf1467b8a77806265?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+      crew: [
+        {
+          name: "Rishab Shetty",
+          job: "Director",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.8bq9m4x1NvuEhtHVrYVF8RKnmJav4Ph_AhYiKdOhtg4?w=88&h=88&c=12&rs=2&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://d9s3e2ooa0od7.cloudfront.net/assets/thalaivar-thambi-thalaimaiyil/1e8f9a70-4d0a-44dd-8ea9-b0c456189246/1e8f9a70-4d0a-44dd-8ea9-b0c456189246.jpg",
+    genre_ids: [18],
+    id: 201006,
+    original_language: "ta",
+    original_title: "Thalaivar Thambi Thalaimaiyil",
+    overview: "Family leadership drama.",
+    popularity: 160.44,
+    poster_path:
+      "https://d9s3e2ooa0od7.cloudfront.net/assets/thalaivar-thambi-thalaimaiyil/1e8f9a70-4d0a-44dd-8ea9-b0c456189246/1e8f9a70-4d0a-44dd-8ea9-b0c456189246.jpg",
+    release_date: "2026-01-15",
+    title: "Thalaivar Thambi Thalaimaiyil",
+    video: false,
+    vote_average: 7.1,
+    vote_count: 520,
+    credits: {
+      cast: [
+        {
+          name: "Jiiva",
+          character: "Politician",
+          profile_path:
+            "https://i0.wp.com/www.primesworld.com/wp-content/uploads/Jiiva-Biography-Height-Weight-Age-Movies-Wife-Family-Salary-Net-Worth-Facts-More.webp",
+        },
+      ],
+      crew: [
+        {
+          name: "Nithish Sahadev",
+          job: "Director",
+          profile_path:
+            "https://cdn-icons-png.flaticon.com/512/1077/1077114.png",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://tse1.mm.bing.net/th/id/OIP.JoNGF2H6Mv4GFjiTFELmuwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    genre_ids: [18],
+    id: 201007,
+    original_language: "ta",
+    original_title: "Parasakthi",
+    overview: "Classic social justice drama.",
+    popularity: 380.67,
+    poster_path:
+      "https://tse1.mm.bing.net/th/id/OIP.JoNGF2H6Mv4GFjiTFELmuwHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    release_date: "2026-01-10",
+    title: "Parasakthi",
+    video: false,
+    vote_average: 8.9,
+    vote_count: 5100,
+    credits: {
+      cast: [
+        {
+          name: "Sivakarthikeyan",
+          character: "Chezhiyan",
+          profile_path:
+            "https://tse3.mm.bing.net/th/id/OIP.AjxOBMs54cT81nyvqNasVgHaIr?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+        {
+          name: "Sreeleela",
+          character: "Ratnamala",
+          profile_path:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Sreeleela_interview.png/440px-Sreeleela_interview.png",
+        },
+        {
+          name: "Ravi Mohan",
+          character: "Thirunaadan",
+          profile_path:
+            "https://www.iwmbuzz.com/wp-content/uploads/2025/05/actor-ravi-mohan-finally-opens-up-says-live-and-let-live-7.jpg",
+        },
+      ],
+      crew: [
+        {
+          name: "Sudha Kongara",
+          job: "Director",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.e505d64ca99efbed6176d3793d5562c1?w=200&h=200&c=12&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://tse1.mm.bing.net/th/id/OIP.Uy7KaabANJ7lINSM_-PO1gHaLG?rs=1&pid=ImgDetMain&o=7&rm=3",
+    genre_ids: [28, 878, 53],
+    id: 201008,
+    original_language: "ta",
+    original_title: "The Greatest Of All Time",
+    overview: "Action thriller on destiny.",
+    popularity: 610.21,
+    poster_path:
+      "https://tse1.mm.bing.net/th/id/OIP.Uy7KaabANJ7lINSM_-PO1gHaLG?rs=1&pid=ImgDetMain&o=7&rm=3",
+    release_date: "2024-09-05",
+    title: "The Greatest Of All Time",
+    video: false,
+    vote_average: 7.6,
+    vote_count: 2100,
+    credits: {
+      cast: [
+        {
+          name: "Vijay",
+          character: "Gandhi",
+          profile_path:
+            "https://m.media-amazon.com/images/M/MV5BZWJlODhlMzctOTU0Yi00MTUwLTkxODYtMDNjNTQxYTI2YTE1XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+        },
+        {
+          name: "Prashanth",
+          character: "Sunil Thiagarajan",
+          profile_path:
+            "https://www.filmibeat.com/img/2018/01/prasanth2-15-1516008158.jpg",
+        },
+      ],
+      crew: [
+        {
+          name: "Venkat Prabhu",
+          job: "Director",
+          profile_path:
+            "https://tse4.mm.bing.net/th/id/OIP.dBoYjbrQDdSVaKRPu7Yx4gHaEU?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://tse4.mm.bing.net/th/id/OIP.NNAszi1l6TU_79zVD6wa-gHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    genre_ids: [28, 18, 53],
+    id: 201009,
+    original_language: "ta",
+    original_title: "Leo",
+    overview: "Man forced back into violence.",
+    popularity: 780.9,
+    poster_path:
+      "https://tse4.mm.bing.net/th/id/OIP.NNAszi1l6TU_79zVD6wa-gHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    release_date: "2023-10-19",
+    title: "Leo",
+    video: false,
+    vote_average: 7.4,
+    vote_count: 8600,
+    credits: {
+      cast: [
+        {
+          name: "Vijay",
+          character: "Leo Das",
+          profile_path:
+            "https://m.media-amazon.com/images/M/MV5BZWJlODhlMzctOTU0Yi00MTUwLTkxODYtMDNjNTQxYTI2YTE1XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
+        },
+        {
+          name: "Trisha",
+          character: "Sathya",
+          profile_path:
+            "https://th.bing.com/th/id/OIP.nFj9emMUvZiJJxTMjeY_HAHaJ4?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+      crew: [
+        {
+          name: "Lokesh Kanagaraj",
+          job: "Director",
+          profile_path:
+            "https://tse2.mm.bing.net/th/id/OIP.1mO5BW_IOiIV321knqoX6AHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://m.media-amazon.com/images/M/MV5BMDZiNzAwZTQtYWIwMC00ODA0LWJiOGMtZTgzZGYzYzMxMDNiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    genre_ids: [28, 53],
+    id: 201010,
+    original_language: "kn",
+    original_title: "Toxic",
+    overview: "Dark gangster world.",
+    popularity: 540.88,
+    poster_path:
+      "https://m.media-amazon.com/images/M/MV5BMDZiNzAwZTQtYWIwMC00ODA0LWJiOGMtZTgzZGYzYzMxMDNiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    release_date: "2026-03-19",
+    title: "Toxic",
+    video: false,
+    vote_average: 7.8,
+    vote_count: 1700,
+    credits: {
+      cast: [
+        {
+          name: "Yash",
+          character: "Gang Leader",
+          profile_path:
+            "https://tse3.mm.bing.net/th/id/OIP.iuPh9Vx_4ufP2rCnem0ujQHaKn?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+      crew: [
+        {
+          name: "Geethu Mohandas",
+          job: "Director",
+          profile_path:
+            "https://th.bing.com/th/id/OSK.2cGliPDGkgOj5oYmn3YZoqkbkDToOvkhd2XpBFluYRY?w=200&h=200&c=12&o=6&dpr=1.5&pid=SANGAM",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://tse1.mm.bing.net/th/id/OIP.YqUMKFj7D_KQblLK-BJlKgHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    genre_ids: [18],
+    id: 201011,
+    original_language: "ta",
+    original_title: "Idli Kadai",
+    overview: "Life around a small food shop.",
+    popularity: 140.22,
+    poster_path:
+      "https://tse1.mm.bing.net/th/id/OIP.YqUMKFj7D_KQblLK-BJlKgHaLH?rs=1&pid=ImgDetMain&o=7&rm=3",
+    release_date: "2025-10-02",
+    title: "Idli Kadai",
+    video: false,
+    vote_average: 7.0,
+    vote_count: 430,
+    credits: {
+      cast: [
+        {
+          name: "Dhanush",
+          character: "Murugan",
+          profile_path:
+            "https://tse2.mm.bing.net/th/id/OIP.rSFrTaOI8VcX5VVZNmzkKgHaEy?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+        {
+          name: "Nithya Menen",
+          character: "Kayal",
+          profile_path:
+            "https://yojanastatus.com/wp-content/uploads/2024/01/Nithya_Menen_2023_Alt-1365x2048.jpg",
+        },
+      ],
+      crew: [
+        {
+          name: "Dhanush",
+          job: "Director",
+          profile_path:
+            "https://tse2.mm.bing.net/th/id/OIP.rSFrTaOI8VcX5VVZNmzkKgHaEy?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+    },
+  },
+  {
+    adult: false,
+    backdrop_path:
+      "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/aaromaley-et00468371-1762256128.jpg",
+    genre_ids: [18, 10749],
+    id: 201012,
+    original_language: "ta",
+    original_title: "Aaromaley",
+    overview: "Poetic romantic drama.",
+    popularity: 175.66,
+    poster_path: "/aaromaley_poster.jpg",
+    release_date: "2025-11-10",
+    title: "Aaromaley",
+    video: false,
+    vote_average: 7.5,
+    vote_count: 690,
+    credits: {
+      cast: [
+        {
+          name: "Kishen Das",
+          character: "Hero",
+          profile_path:
+            "https://images.filmibeat.com/img/popcorn/movie_posters/aaromaley-20250530123803-23730.jpg",
+        },
+        {
+          name: "Harshath khan",
+          character: "Friend",
+          profile_path:
+            "https://tse4.mm.bing.net/th/id/OIP.d9YrGkS8InyJjHesOg-FbwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3",
+        },
+      ],
+      crew: [
+        {
+          name: "Sarang Thiagu",
+          job: "Director",
+          profile_path:
+            "https://static.vecteezy.com/system/resources/previews/000/550/731/original/user-icon-vector.jpg",
+        },
+      ],
+    },
+  },
+];
+
+export default data;
