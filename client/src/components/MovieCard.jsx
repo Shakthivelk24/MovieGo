9022cea574af2538a2bd2de37692293ba9fa098a
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-04 21:13:34 +0530

    feat: enhance MovieCard styling and layout for improved user experience

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,29 +1,57 @@
-import { StarIcon } from 'lucide-react';
-import { useNavigate } from 'react-router-dom'
-import timeFormat from '../lib/timeFormat';
+import { StarIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import timeFormat from "../lib/timeFormat";
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
   return (
-    <div className='flex flex-col justify-between p-2 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
-       <img  onClick={() =>{ scrollTo(0,0); navigate(`/movies/${movie._id}`)}}
-       src={movie.backdrop_path} alt={movie.title} className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer'/>
-       <p className='font-semibold mt-2 truncate'>{movie.title}</p>
-       <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()} . {movie.genres.slice(0,2).map(g => g.name).join(" | ")} . {timeFormat(movie.runtime)}
-       </p>
-       <div className='flex items-center justify-between mt-4 pb-3'>
-        <button
-           className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'
-           onClick={() =>{ scrollTo(0,0); navigate(`/movies/${movie._id}`)}}
-        >Buy Tickets</button>
-        <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
-            <StarIcon className='w-4 h-4 text-primary fill-primary'/>
-            {movie.vote_average.toFixed(1)}
-        </p>
-       </div>
-    </div>
-  )
-}
+    <div
+      className="flex flex-col justify-between p-4 bg-slate-800 rounded-3xl 
+hover:-translate-y-1 transition-all duration-300 w-64 shadow-lg"
+    >
+      <img
+        onClick={() => {
+          scrollTo(0, 0);
+          navigate(`/movies/${movie._id}`);
+        }}
+        src={movie.backdrop_path}
+        alt={movie.title}
+        className="rounded-2xl h-56 w-full object-cover cursor-pointer"
+      />
 
-export default MovieCard
+      <p className="font-semibold text-lg mt-3 truncate text-white">
+        {movie.title}
+      </p>
+
+      <p className="text-sm text-gray-400 mt-1">
+        {new Date(movie.release_date).getFullYear()}{" "}{" "}
+        {movie.genres
+          .slice(0, 2)
+          .map((g) => g.name)
+          .join(" | ")}{" "}
+         {timeFormat(movie.runtime)}
+      </p>
+
+      <div className="flex items-center justify-between mt-4">
+        <button
+          onClick={() => {
+            scrollTo(0, 0);
+            navigate(`/movies/${movie._id}`);
+          }}
+          className="px-5 py-2 text-sm bg-pink-500 hover:bg-pink-600 
+      transition rounded-full font-medium text-white cursor-pointer"
+        >
+          Buy Tickets
+        </button>
+
+        <p className="flex items-center gap-1 text-sm text-gray-300">
+          <StarIcon className="w-4 h-4 text-pink-500 fill-pink-500" />
+
+          {movie.vote_average.toFixed(1)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
