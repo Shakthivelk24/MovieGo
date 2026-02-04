64596a72dc12df43c670b9a615c2a519176397ca
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-04 21:13:52 +0530

    feat: update Favorite component to display user favorites from context

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { dummyShowsData } from '../assets/assets'
 import MovieCard from '../components/MovieCard.jsx'
 import BlurCircle from '../components/BlurCircle';
+import { useAppContext } from '../context/AppContext';
 
 const Favorite = () => {
-  return dummyShowsData.length > 0 ? (
+  const { favorites } = useAppContext();
+
+  console.log("Favorite movies:", favorites);
+
+  return favorites.length > 0 ? (
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
         <BlurCircle top='150px' left='0px'/>
         <BlurCircle top='500px' right='50px'/>
         <h1 className='text-lg font-medium my-4'>Your Favorite Movies</h1>
         <div className='flex flex-wrap max-sm:justify-center gap-8'>
-          {dummyShowsData.map((movie) =>(
+          {favorites.map((movie) =>(
              <MovieCard movie={movie} key={movie._id}/>
           ))}
         </div>
